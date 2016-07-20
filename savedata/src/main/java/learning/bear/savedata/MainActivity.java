e9bacc351320eb6df05b617c93372e78cdbe54b0
Pengyu Zhang <965006619@qq.com>
2016-07-20 23:56:00 +0800

    Add SaveData demo testInternalStorage

--- savedata/src/main/java/learning/bear/savedata/MainActivity.java
+++ savedata/src/main/java/learning/bear/savedata/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -16,33 +19,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = this.getSharedPreferences(
-                    getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        //SharedPreferences sharedPref = getPreferences(
-        //            Context.MODE_PRIVATE);
+        testSharedPreferences();
+        testInternalStorage();
+    }
 
-
-
-
+    void testSharedPreferences(){
+        //SharedPreferences sharedPreferences = this.getSharedPreferences(
+        //        getString(R.string.preference_file_key),MODE_PRIVATE);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-
-        int defaultValue = Integer.parseInt(getString(R.string.boot_times_default));
-        //int defaultValue = 0;
-        int bootTimes = sharedPref.getInt(getString(R.string.boot_times), defaultValue);
-
-
+        int bootTimes;
+        bootTimes = sharedPref.getInt(getString(R.string.saved_bootTimes), 0);
         bootTimes++;
-
-        editor.putInt(getString(R.string.boot_times),bootTimes);
+        editor.putInt(getString(R.string.saved_bootTimes), bootTimes);
         editor.apply();
 
         TextView textView = (TextView)findViewById(R.id.showBootTimes);
-        String string = "Boot times : " + bootTimes + ".";
+        String string = "Boot times : "+bootTimes+"(By SharedPreferences).";
         textView.setText(string);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+    void testInternalStorage(){
+        //File file = new File(this.getFilesDir(),getString(R.string.internalStorageFileName));
+        FileOutputStream outputStream;
+        FileInputStream inputStream;
+        String filename = getString(R.string.internalStorageFileName);
+        int writeFileValue = 36;
+        try{
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(writeFileValue);
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        writeFileValue = 0;
+        try{
+            inputStream = openFileInput(filename);
+            writeFileValue = inputStream.read();
+            inputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        TextView textView = (TextView)findViewById(R.id.showBootTimes);
+        String string = textView.getText().toString()+"\nWriteReadTest："
+                +writeFileValue+"(By InternalStorage).";
+        textView.setText(string);
     }
 }
