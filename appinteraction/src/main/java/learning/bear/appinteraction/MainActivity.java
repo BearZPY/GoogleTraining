77e9d2e9fc7851346b75ff168a7ef65793b168bc
BearZPY <965006619@qq.com>
2016-07-26 18:24:52 +0800

    Add app interaction get contact tel number

--- appinteraction/src/main/java/learning/bear/appinteraction/MainActivity.java
+++ appinteraction/src/main/java/learning/bear/appinteraction/MainActivity.java
@@ -3,16 +3,21 @@ package learning.bear.appinteraction;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.open_map);
         button.setOnClickListener(buttonClick);
         button = (Button) findViewById(R.id.send_email);
+        button.setOnClickListener(buttonClick);
+        button = (Button) findViewById(R.id.get_contact);
         button.setOnClickListener(buttonClick);
     }
 
@@ -45,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case R.id.send_email:
                     sendEmail();
+                    break;
+                case R.id.get_contact:
+                    getContact();
                     break;
                 default:break;
             }
@@ -118,5 +128,51 @@ public class MainActivity extends AppCompatActivity {
         // emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message text");
         // emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
         // You can also attach multiple items by passing an ArrayList of Uris
+    }
+
+    void getContact(){
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check which request it is that we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                String number = "None";
+                try{
+                    cursor.moveToFirst();
+                    // Retrieve the phone number from the NUMBER column
+                    int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    number = cursor.getString(column);
+                    cursor.close();
+                    // Do something with the phone number...
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                TextView textView = (TextView) findViewById(R.id.showNumber);
+                String string = textView.getText().toString() +
+                        "\nThe Number is " + number+ ".";
+                textView.setText(string);
+            }
+        }
     }
 }
