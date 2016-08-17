3a0cc5b7d275b95244f06564ea5b1a162a5a7f10
BearZPY <965006619@qq.com>
2016-08-17 23:31:17 +0800

    modify app interaction share file

--- appinteraction/src/main/java/learning/bear/appinteraction/ShareFileActivity.java
+++ appinteraction/src/main/java/learning/bear/appinteraction/ShareFileActivity.java
@@ -1,14 +1,15 @@
 package learning.bear.appinteraction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ShareFileActivity extends AppCompatActivity {
 
@@ -17,28 +18,35 @@ public class ShareFileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_file);
 
-        File docPath = new File(this.getFilesDir(), "docs");
-        File docFile = new File(docPath, "test.txt");
-        FileOutputStream outputStream;
-        try {
-            outputStream = openFileOutput(docFile.getName(), Context.MODE_PRIVATE);
-            outputStream.write(36);
+        File path = new File(getFilesDir(),"docs");
+        File docFile = new File(path,"myTest.txt");
+        if(path.mkdirs()){
+            Toast.makeText(this, "新建docs文件夹"
+                    , Toast.LENGTH_LONG).show();
+        }
+
+
+        try{
+            String testString = "This is a test.txt file.";
+            byte testByte[] = testString.getBytes();
+
+            FileOutputStream outputStream = new FileOutputStream(docFile);
+            outputStream.write(testByte);
             outputStream.close();
-        } catch (Exception e) {
+
+        }catch (IOException e){
             e.printStackTrace();
         }
         //boolean flag = docFile.exists();
-        //if(!docFile.exists())
-
-        Uri docUri = FileProvider.getUriForFile(
-                ShareFileActivity.this,
+        Uri contentUri = FileProvider.getUriForFile(this,
                 "learning.bear.appinteraction.fileprovider",
                 docFile);
-        if(docUri != null){
-            Intent docIntent = new Intent(
-                    Intent.ACTION_VIEW,docUri);
-            docIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(Intent.createChooser(docIntent,"Share txt."));
-        }
+
+
+        Intent shareIntent = new Intent();
+        shareIntent.setData(contentUri);
+        shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(shareIntent,"Share myTest.txt"));
+        finish();
     }
 }
