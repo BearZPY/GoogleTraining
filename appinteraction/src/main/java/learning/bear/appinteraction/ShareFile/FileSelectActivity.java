bcf0955b1c4ed1b8921041bc471b6fd9af8f21cb
BearZPY <965006619@qq.com>
2016-08-17 15:04:14 +0800

    add app interaction share file

---
+++ appinteraction/src/main/java/learning/bear/appinteraction/ShareFile/FileSelectActivity.java
@@ -0,0 +1,52 @@
+package learning.bear.appinteraction.shareFile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
+
+import java.io.File;
+
+import learning.bear.appinteraction.MainActivity;
+import learning.bear.appinteraction.R;
+
+public class FileSelectActivity extends Activity {
+
+    // The path to the root of this app's internal storage
+    private File mPrivateRootDir;
+    // The path to the "images" subdirectory
+    private File mImagesDir;
+    // Array of files in the images subdirectory
+    File[] mImageFiles;
+    // Array of filenames corresponding to mImageFiles
+    String[] mImageFilenames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_select);
+
+        // Set up an Intent to send back to apps that request a file
+        Intent mResultIntent =
+                new Intent("com.example.myapp.ACTION_RETURN_FILE");
+        // Get the files/ subdirectory of internal storage
+        mPrivateRootDir = getFilesDir();
+        // Get the files/images subdirectory;
+        mImagesDir = new File(mPrivateRootDir, "images");
+        // Get the files in the images subdirectory
+        mImageFiles = mImagesDir.listFiles();
+        // Set the Activity's result to null to begin with
+        setResult(Activity.RESULT_CANCELED, null);
+        /*
+         * Display the file names in the ListView mFileListView.
+         * Back the ListView with the array mImageFilenames, which
+         * you can create by iterating through mImageFiles and
+         * calling File.getAbsolutePath() for each File
+         */
+    }
+
+
+}
--- appinteraction/src/main/java/learning/bear/appinteraction/ShareFileActivity.java
+++ appinteraction/src/main/java/learning/bear/appinteraction/ShareFileActivity.java
@@ -1,7 +1,14 @@
 package learning.bear.appinteraction;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class ShareFileActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class ShareFileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_file);
+
+        File docPath = new File(this.getFilesDir(), "docs");
+        File docFile = new File(docPath, "test.txt");
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(docFile.getName(), Context.MODE_PRIVATE);
+            outputStream.write(36);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //boolean flag = docFile.exists();
+        //if(!docFile.exists())
+
+        Uri docUri = FileProvider.getUriForFile(
+                ShareFileActivity.this,
+                "learning.bear.appinteraction.fileprovider",
+                docFile);
+        if(docUri != null){
+            Intent docIntent = new Intent(
+                    Intent.ACTION_VIEW,docUri);
+            docIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(Intent.createChooser(docIntent,"Share txt."));
+        }
     }
 }
